2306957fac56c009932a2adef9109a6814143cc1
DucTrong2003 <125754850+DucTrong2003@users.noreply.github.com>
2023-05-22 16:01:29 +0700

    Update and rename KhachHangNuocNgoai.java to B2_KH_NuocNgoai.java

--- B2_KH_NuocNgoai.java
+++ B2_KH_NuocNgoai.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package newpackage1;
 
-/**
- *
- * @author ADMIN
- */
 class KhachHangNuocNgoai extends HoaDon {
     private String quocTich;
     private double soLuong;
