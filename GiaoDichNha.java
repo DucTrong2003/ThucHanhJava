046a4d20452111dd6fdb78fcdb6bd68330a919df
DucTrong2003 <125754850+DucTrong2003@users.noreply.github.com>
2023-05-22 15:55:34 +0700

    Update GDNha.java

--- GiaoDichNha.java
+++ GiaoDichNha.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package newpackage;
 
-/**
- *
- * @author ADMIN
- */
 class GiaoDichNha extends GiaoDich {
     private String loaiNha;
     private String diaChi;
